f5bd07429f7556a976965ca63b4ca1703d678e71
DouglasGo8 <abraham.dias.douglas@gmail.com>
2023-06-16 20:10:36 -0300

    Section04 done

---
+++ app-service-s04v1/src/main/java/com/udemy/spring/security/service/session04/ServiceSession02App.java
@@ -0,0 +1,15 @@
+package com.udemy.spring.security.service.session04;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Service Module Session 03
+ */
+//@EnableWebSecurity optional annotation
+@SpringBootApplication
+public class ServiceSession02App {
+  public static void main(String[] args) {
+    SpringApplication.run(ServiceSession02App.class, args);
+  }
+}
---
+++ app-service-s04v1/src/test/java/com/udemy/spring/security/service/session04/ServiceSession02AppTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring.security.service.session02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceSession02AppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
